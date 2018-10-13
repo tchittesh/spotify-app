664de139cc738bb8c2e2daddff75124e7743c8af
Chittesh Ruthesh <Sumathi@Chitteshs-MacBook-Pro.local>
2018-10-13 16:56:02 -0400

    host button works

--- server.js
+++ server.js
@@ -8,140 +8,108 @@
  */
 
 var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" library
-var cors = require('cors');
-var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
+var fs = require('fs');
+var path = require('path');
+var listofrooms;
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var client_id = 'c4afbd083ef34b34810c5b3c16a2e08d'; // Your client id
+// var client_secret = 'CLIENT_SECRET'; // Your secret
+var redirect_uri = 'http://localhost:8000/callback'; // Your redirect uri
 
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
-var generateRandomString = function(length) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+function getHashParams() {
+  var hashParams = {};
+  var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = location.hash.substring(1);
+  while ( e = r.exec(q)) {
+     hashParams[e[1]] = decodeURIComponent(e[2]);
   }
-  return text;
+  return hashParams;
 };
 
 var stateKey = 'spotify_auth_state';
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'))
-   .use(cors())
-   .use(cookieParser());
+app.use(express.static(__dirname + '/screens'));
 
-app.get('/login', function(req, res) {
-
-  var state = generateRandomString(16);
-  res.cookie(stateKey, state);
-
-  // your application requests authorization
-  var scope = 'user-read-private user-read-email';
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
+app.get('/', function(req, res) {
+  res.sendFile(__dirname+'/screens/main_or.html');
 });
+
+app.get('/loggedinh', function(req, res) {
+  userinfo = JSON.parse(req.query.userinfo);
+  fs.readFile('data/listofrooms.txt', function(err, data) {
+    if (err) throw err;
+    listofrooms = JSON.parse(data);
+    roomcode = "";
+    while (roomcode == "" || listofrooms.roomcodes.includes(roomcode)){
+      roomcode = "";
+      var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      for (var i = 0; i < 5; i++) {
+        roomcode += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+    }
+    listofrooms.roomcodes.push(roomcode);
+    console.log(userinfo);
+    console.log(JSON.stringify(listofrooms));
+    // tag = "user1"+req.query.type;
+    roomdata = {"numusers": 1, "users": {"user1": req.query.userinfo}}
+    fs.writeFile('data/'+roomcode+'.txt', JSON.stringify(roomdata), function (err) {
+      if (err) throw err;
+      console.log('Saved userinfo!');
+    });
+    fs.writeFile('data/listofrooms.txt', JSON.stringify(listofrooms), function (err) {
+      if (err) throw err;
+      console.log('Saved roomcode!');
+    });
+  });
+  // res.json({"rc" : roomcode});
+  res.end();
+  // res.sendFile(__dirname+'/screens/endh.html');
+});
+
+// app.get('/loggeding', function(req, res) {
+//   userinfo = JSON.parse(req.query.userinfo);
+//   inputrc = req.query.rcode_input;
+//   fs.readFile('data/listofrooms.txt', function(err, data) {
+//     if (err) throw err;
+//     listofrooms = JSON.parse(data);
+//     if (!listofrooms.roomcodes.includes(inputrc)) res.sendFile(__dirname+'/screens/enter_roomcode.html');
+//     fs.readFile('data/'+inputrc+'.txt', function(err, data1) {
+//       if (err) throw err;
+//       roomuserdata = JSON.parse(data1);
+//       roomuserdata.numusers++;
+//       temp = "user"+roomuserdata.numusers;
+//       roomuserdata.users.temp = userinfo;
+//       fs.writeFile('data/'+inputrc+'.txt', JSON.stringify(roomuserdata), function(err) {
+//         if (err) throw err;
+//         console.log('updated data/'+inputrc+'.txt');
+//       });
+//     });
+//   res.sendFile(__dirname+'/screens/loggedin.html');
+// });
 
 app.get('/callback', function(req, res) {
-
-  // your application requests refresh and access tokens
-  // after checking the state parameter
-
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
-
-  if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
-  } else {
-    res.clearCookie(stateKey);
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
-
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
-
-        var options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
-
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log(body);
-        });
-
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
-      } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
-      }
-    });
-  }
+  res.sendFile(__dirname+'/screens/use_access_token.html');
 });
 
-app.get('/refresh_token', function(req, res) {
+app.get('/endhost', function(req, res) {
+  res.sendFile(__dirname+'/screens/endh.html');
+})
 
-  // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
 
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
-      res.send({
-        'access_token': access_token
-      });
-    }
+
+files = fs.readdirSync(__dirname+'/data');
+for (const file of files) {
+  console.log('deleted '+path.join('data', file));
+  fs.unlinkSync(path.join('data', file), err => {
+    if (err) throw err;
   });
+}
+listofrooms = {"roomcodes": []};
+fs.writeFile('data/listofrooms.txt', JSON.stringify(listofrooms), function (err) {
+  if (err) throw err;
+  console.log('initialized data/listofrooms.txt!');
 });
-
-console.log('Listening on 8888');
-app.listen(8888);
+console.log('Listening on 8000');
+app.listen(8000);
